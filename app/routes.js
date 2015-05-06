b6d8b1dbe0acbe9e70a8da068d0d54d71c39b280
ballzy <christopher.ball77@gmail.com>
2015-05-06 16:22:38 +0100

    Full routes.js for Guided TMS

--- app/routes.js
+++ app/routes.js
@@ -29,7 +29,58 @@ module.exports = {
 
       res.render('story-gen/eating-drinking-more',req.body);
     });
-      
+
+    app.post('/story-gen/moving-around-more', function (req, res) {
+
+      res.render('story-gen/moving-around-more',req.body);
+    });
+
+    app.post('/story-gen/mixing-with-people-more', function (req, res) {
+
+      res.render('story-gen/mixing-with-people-more',req.body);
+    });
+
+    app.post('/story-gen/comunicating-more', function (req, res) {
+
+      res.render('story-gen/communicating-more',req.body);
+    });
+
+    app.post('/story-gen/managing-treatments-more', function (req, res) {
+
+      res.render('story-gen/managing-treatments-more',req.body);
+    });
+
+    app.post('/story-gen/going-out-more', function (req, res) {
+
+      res.render('story-gen/going-out-more',req.body);
+    });
+    
+    app.post('/story-gen/washing-bathing-more', function (req, res) {
+
+      res.render('story-gen/washing-bathing-more',req.body);
+    });
+
+    app.post('/story-gen/going-to-toilet-more', function (req, res) {
+
+      res.render('story-gen/going-to-toilet-more',req.body);
+    });
+
+    app.post('/story-gen/dressing-undressing-more', function (req, res) {
+
+      res.render('story-gen/dressing-undressing-more',req.body);
+    }); 
+
+    app.post('/story-gen/reading-more', function (req, res) {
+
+      res.render('story-gen/reading-more',req.body);
+    });
+
+    app.post('/story-gen/managing-money-more', function (req, res) {
+
+      res.render('story-gen/managing-money-more',req.body);
+    });
+
+
       
   }
 };
