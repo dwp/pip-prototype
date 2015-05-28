488aa1945e20aa8960e7fc90deea81acf62dd44e
Colin Oakley <colin@htmlandbacon.com>
2015-05-28 17:10:41 +0100

    added edit feature and logout

--- app/review_flow.js
+++ app/review_flow.js
@@ -25,10 +25,7 @@ module.exports = function(app){
     function movingToForm(req)
     {
         var formData = new Array;
-        
-        
-        
-        console.log(req.session);
+    
         
         if(req.session['moving']['distance'] == '20'){ formData['distance-20'] = true; }
         if(req.session['moving']['distance'] == '20-50'){ formData['distance-50'] = true; }
@@ -49,45 +46,51 @@ module.exports = function(app){
         formData['wheel-chair'] = req.session['moving']['wheel-chair']
         formData['aids-appliances-details'] = req.session['moving']['aids-appliances-details']
         
-        
-        console.log(formData);
-        
         return formData
     }
     
     app.get('/review-flow/personal', function (req, res) {        
-      res.render('review-flow/personal', {'name': req.session['name']} );
+      res.render('review-flow/personal', {'name': req.session['name'],'edit': req.param('edit')} );
     });   
     
     app.post('/review-flow/personal', function (req, res) {        
         req.session['name'] = req.body.name;
         if(req.body.name != ""){ req.session['nameSet'] = true; }else{ req.session['nameSet'] = false; } 
-        res.redirect('/review-flow/contact');
+        if(req.param('edit')){ 
+            res.redirect('/review-flow/review');
+        }else{
+            res.redirect('/review-flow/contact'); }
     });   
      
     app.get('/review-flow/contact', function (req, res) {
-      res.render('review-flow/contact',{'phone': req.session['phone']});
+      res.render('review-flow/contact',{'phone': req.session['phone'],'edit': req.param('edit')});
     });   
     
     app.post('/review-flow/contact', function (req, res){      
         req.session['phone'] = req.body.phone;
         if(req.body.phone != ""){ req.session['phoneSet'] = true; }else{ req.session['phoneSet'] = false; }      
-        res.redirect('/review-flow/food');
+        if(req.param('edit')){ 
+            res.redirect('/review-flow/review');
+        }else{
+            res.redirect('/review-flow/food'); }
     }); 
     
     app.get('/review-flow/food', function (req, res) {
         if(req.session['food']){ var formData = foodToForm(req) }else{ formData = new Array; }
-      res.render('review-flow/food',{'form': formData});
+      res.render('review-flow/food',{'form': formData,'edit': req.param('edit')});
     });   
     
     app.post('/review-flow/food', function (req, res) {
         req.session['food'] = req.body;
-        res.redirect('/review-flow/moving');
+        if(req.param('edit')){ 
+            res.redirect('/review-flow/review');
+        }else{
+            res.redirect('/review-flow/moving'); }
     }); 
     
     app.get('/review-flow/moving', function (req, res) {
      if(req.session['moving']){ var formData = movingToForm(req) }else{ formData = new Array; }
-      res.render('review-flow/moving',{'form': formData});
+      res.render('review-flow/moving',{'form': formData,'edit': req.param('edit')});
     });   
     
     app.post('/review-flow/moving', function (req, res) { 
@@ -102,32 +105,42 @@ module.exports = function(app){
         
       reviewSection['sectionContact'] = req.session['phoneSet']  
       reviewSection['sectionName'] = req.session['nameSet'] 
-      reviewSection['sectionFood'] = foodToForm(req)
-      reviewSection['sectionMoving'] = movingToForm(req);
       
-        //set section values //
-      if(req.session['name']){  reviewSection['name'] = req.session['name']; }else { reviewSection['name'] = "N/a" }
-      if(req.session['phone']){ reviewSection['phone'] = req.session['phone']; }else { reviewSection['phone'] = "N/a" }
-    
-    if( req.session['moving']['aids-appliances'] != "" 
+      if(req.session['food']){ 
+          reviewSection['sectionFood'] = foodToForm(req) 
+          if( req.session['food']['aids-appliances'] != "" 
         && req.session['food']['another-person'] !=""
         && req.session['food']['cook-other'] !=""){
         reviewSection['sectionFoodComplete'] = true;
     }else{ reviewSection['sectionFoodComplete'] = false; }
+      
+      }
         
-        
-        if(req.session['moving']['distance'] != "" && 
+      if(req.session['moving'])
+      {
+          reviewSection['sectionMoving'] = movingToForm(req); 
+                  if(req.session['moving']['distance'] != "" && 
             req.session['moving']['aids-appliances'] != "" &&
             req.session['moving']['wheelchair']  != "" &&
             req.session['moving']['aids-appliances-details'] !=""){
                 reviewSection['sectionMovingComplete'] = true;
             }else{ reviewSection['sectionMovingComplete'] = false; }
-        
-       //  console.log(req.session)
-       
-         
-          console.log(reviewSection)
+      }
+     
+      
+        //set section values //
+      if(req.session['name']){  reviewSection['name'] = req.session['name']; }else { reviewSection['name'] = "N/a" }
+      if(req.session['phone']){ reviewSection['phone'] = req.session['phone']; }else { reviewSection['phone'] = "N/a" }
+    
         
       res.render('review-flow/review',reviewSection);
     });    
+    
+    
+    app.get('/review-flow/logout', function (req, res) {
+            req.session.destroy();
+            res.redirect('/review-flow/personal');
+    });   
+    
+    
 }
